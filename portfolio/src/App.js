b7adb1b7e7adc86e1144fa86b6f9dd1e48eb5f16
Rahul phatangare <rahulphatangare01@gmail.com>
2023-12-27 22:05:52 +0530

    created simple component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, {ThemeProvider} from 'styled-components';
+import {darkTheme} from './utils/themes/themes';
+import HeroSection from './components/pages/HeroSection';
+import Skills from './components/pages/Skills';
+import Navbar from './components/pages/Navbar';
+import Education from './components/pages/Education';
+
+
+const Body  = styled.div`
+background-color:${({theme})=>theme.bg};
+width:100%;
+height:100%;
+overflow-x:hidden;
+`;
+
+const Wrapper = styled.div`
+background:linear-gradient(
+  38.73deg,
+  rgba(204, 0, 187, 0.15) 0%,
+  rgba(201, 32, 184, 0) 50%,
+  ),
+
+  linear-gradient(
+    141.27deg,
+    rgba(0, 70, 209, 0) 50%,
+    rgba(0, 70, 209, 0.15) 100%
+  );
+  width:100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%); 
+  `;
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>
-      Portfolio
-     </h1>
-    </div>
+  
+  <ThemeProvider theme={darkTheme}>
+    <Navbar/>
+     <Body>
+      <HeroSection/>
+      <Wrapper>
+      <Skills/>
+      <Education/>
+      </Wrapper>
+     </Body>
+    </ThemeProvider>
   );
 }
 
